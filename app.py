d5c85d623540f4f5b3536a1a28a9a03de05a1a3f
juliadfg <jkranelli@gmail.com>
2026-03-17 13:17:43 -0300

    init

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, url_for, flash, request
 from database import db_session, Funcionario
 from sqlalchemy import select, and_, func
 from flask_login import LoginManager, login_required, login_user, logout_user, current_user
+from api_routes import routes
 
 app = Flask(__name__)
 # mover para .env
@@ -308,6 +309,16 @@ def cadastrar():
 @app.route('/animais')
 def animais():
     return render_template("animais.html")
+
+@app.route('/gatos')
+def listar_gatos():
+    gatos = routes.get_gatos()
+
+
+    for gato in gatos:
+        gato["temperament"] = gato["temperament"].split(',')
+        gato ["image"] = routes.get_image()["url"]
+    return render_template("gatos.html", gatos=gatos)
 
 
 # TODO Final do código
